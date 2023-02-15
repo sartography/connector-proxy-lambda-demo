375738be9ed12317975465fbcfdef5a0be6b88ee
Jon Herron <jon.herron@yahoo.com>
2023-02-14 21:18:35 -0500

    Adding tests

--- connector_proxy_lambda_demo/lambda_function.py
+++ connector_proxy_lambda_demo/lambda_function.py
@@ -1,7 +1,30 @@
 import json
 
 def lambda_handler(event, context):
+    raw_path = event.get("rawPath", "")
+    result = {
+        "statusCode": 404,
+        "body": "Not found."
+    }
+    if raw_path == "/":
+        result = _index(event, context)
+    elif raw_path == "/v1/commands":
+        result = _commands(event, context)
+
+    result["body"] = json.dumps(result["body"])
+
+    return result
+
+def _index(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps("Uploaded a zip!")
+        "body": "This is the SpiffWorkflow Connector.   Point SpiffWorkfow-backend configuration to this url." \
+           " Please see /v1/commands for a list of commands this connector proxy will allow."
     }
+
+def _commands(event, context):
+    return {
+        "statusCode": 200,
+        "body": []
+    }
+
---
+++ tests/unit/lambda_function_test.py
@@ -0,0 +1,25 @@
+import json
+
+from connector_proxy_lambda_demo.lambda_function import lambda_handler
+
+def test_index():
+    event = {'version': '2.0', 'routeKey': '$default', 'rawPath': '/', 'rawQueryString': ''}
+    context = {}
+    result = lambda_handler(event, context)
+    assert result["statusCode"] == 200
+    body = json.loads(result["body"])
+    assert body.startswith("This is the SpiffWorkflow Connector.")
+
+def test_404():
+    event = {'version': '2.0', 'routeKey': '$default', 'rawPath': '/bob', 'rawQueryString': ''}
+    context = {}
+    result = lambda_handler(event, context)
+    assert result["statusCode"] == 404
+    assert result["body"] == '"Not found."'
+
+def test_v1_commands():
+    event = {'version': '2.0', 'routeKey': '$default', 'rawPath': '/v1/commands', 'rawQueryString': ''}
+    context = {}
+    result = lambda_handler(event, context)
+    assert result["statusCode"] == 200
+    assert result["body"] == '[]'
